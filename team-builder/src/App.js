26c7631d44c1ea79a1d319615d473faab1965326
Adam Pitchie <adampitchie@gmail.com>
2021-03-23 13:47:24 -0700

    working

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import Form from './Form'
+
+const teamList = [
+  { username: 'john', email: 'john@abc.com', role: 'backend engineer' },
+  { username: 'sally', email: 'sally@abc.com', role: 'frontend engineer' }
+]
+
+const initialFormValues = { username: '', email: '', role: '' }
 
 function App() {
+
+  const [members, setMembers] = useState(teamList)
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const updateForm = evt => {
+    const { name, value } = evt.target
+    setFormValues({...formValues, [name]: value})
+  }
+
+  const submitForm = evt => {
+    evt.preventDefault();
+    setMembers([...members, formValues])
+    setFormValues(initialFormValues)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Team Members</h1>
+      <Form 
+        values={members}
+        update={updateForm}
+        submit={submitForm}
+      />
     </div>
   );
 }
